b02a29b5879a44586ef308743b7dc9eb093089f7
marbiru <code@uribram.com>
2016-11-05 10:54:06 +0200

    new problems

---
+++ js/problems.js
@@ -0,0 +1,5 @@
+var problems = [
+["What is the annual GDP of China?", "US Dollars", 9240000000000],
+["What is the population of Indonesia?", "people", 249900000 ],
+["What is the market cap of Amazon?", "US Dollars", 358770000000]
+]
--- js/questions.js
+++
@@ -1,5 +0,0 @@
-var problems = [
-["What is the annual GDP of the United States?", "US Dollars", 18650000000000],
-["What is the population of Brazil?", "people", 206440850 ],
-["What is the market cap of Microsoft?", "US Dollars", 460400000000]
-]
